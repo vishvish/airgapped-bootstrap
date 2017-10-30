93b9c5f0a8ec71fd4195de3359e3757a70f4bb09
Vish Vishvanath <vishvish@users.noreply.github.com>
2017-10-30 22:28:20 +0000

    Install docker, compose and awx offline

--- 002_offline/script.sh
+++ 002_offline/script.sh
@@ -7,7 +7,20 @@ sudo /sbin/route del default
 # Copy repo definition
 sudo cp /shared_packages/bootstrap.repo /etc/yum.repos.d/
 
-sudo yum install --disablerepo="*" --enablerepo="bootstrap" python-pip
+sudo yum -y install --disablerepo="*" --enablerepo="bootstrap" python-pip
 
-sudo yum install --disablerepo="*" --enablerepo="bootstrap" /shared_packages/docker/docker-17.09.0.rpm
+sudo yum -y install --disablerepo="*" --enablerepo="bootstrap" /shared_packages/docker/docker-17.09.0.rpm
 
+sudo systemctl start docker
+
+sudo pip install --index-url=file:///shared_packages/pip/simple docker-compose
+
+cd /shared_packages/docker
+sudo docker load -i memcached.docker
+sudo docker load -i postgres.docker
+sudo docker load -i rabbitmq.docker
+sudo docker load -i awx_task.docker
+sudo docker load -i awx_web.docker
+
+cd /shared_dependencies/awx
+docker-compose up -d
